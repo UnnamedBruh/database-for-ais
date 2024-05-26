fce62450a2373f5a82f9b6f6d25db46167e14225
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-05-26 14:12:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,58 +1,120 @@
 async function transitions(json, f) {
 	let i = 0
-	while (!!json[String(i)]) {
+	while (json[String(i)]) {
 		const e = json[String(i)]
 		if (e.startsWith("[") && e.endsWith("]")) {
 			const element = document.createElement("img")
 			const url = e.slice(1, -1)
 			document.body.appendChild(element)
 			element.src = url
-			if (f === false) {element.style.opacity = 0
-			async t() {
-				return new Promise(function(res, rej) {
-					function e() {
-						if (element.style.opacity < 1) {
-							element.style.opacity += 0.1
+			if (!f) {
+				element.style.opacity = 0
+				await new Promise((resolve) => {
+					element.onload = function() {
+						function fadeIn() {
+							const opacity = parseFloat(element.style.opacity)
+							if (opacity < 1) {
+								element.style.opacity = (opacity + 0.1).toString()
+								requestAnimationFrame(fadeIn)
+							} else {
+								resolve()
+							}
+						}
+						fadeIn()
+					}
+
+					element.onerror = function() {
+						element.remove()
+						resolve()
+					}
+				})
+			}
+		} else {
+			let element = document.createElement("p")
+			document.body.appendChild(element)
+			if (!f) {
+				await new Promise((resolve) => {
+					function animateText(index) {
+						if (index < e.length) {
+							if (e[index] === "\n") {
+								document.body.appendChild(document.createElement("br"))
+								element = document.createElement("p")
+								document.body.appendChild(element)
+							} else {
+								element.innerText += e[index]
+							}
+            	requestAnimationFrame(() => animateText(index + 1))
 						} else {
-							res()
+							resolve()
 						}
 					}
-					e()
+					animateText(0)
 				})
+			} else {
+				element.innerText = e
 			}
-			await t()}
-		} else {
-			const element = document.createElement("p")
-			document.body.appendChild(element)
-			if (f === false) {async function anim(s, e, a) {
-				let est = a
-				if (e.length > s) {
-					if (e[s] === "\n") {
-						document.body.appendChild(document.createElement("br"))
-						est = document.createElement("p")
-						document.body.appendChild(est)
-					} else {
-						a.innerText += e[s]
-					}
-					requestAnimationFrame(anim(s + 1, e, est))
-				} else {
-					e()
-				}
-			}
-			async e(i) {
-				return new Promise(function(res, rej) {
-					anim(i, res, element)
-				})
-			}
-			await e(0)
-		}} else {
-			element.innerText = e
 		}
+		i++
 	}
 }
 
-document.querySelector("input").addEventListener("keydown", function(event) {
-	if (event.key === "Enter") {
-		
-	}
-}
+const center = document.createElement("center")
+document.body.appendChild(center)
+const p = document.createElement("p")
+p.innerText = "Apanent167's Wikipedia"
+center.appendChild(p)
+const input = document.createElement("input")
+input.placeholder = "Loading articles..."
+input.type = "text"
+input.style.width = "500px"
+center.appendChild(input)
+const select = document.createElement("select")
+const option = document.createElement("option")
+select.appendChild(option)
+option.innerText = "Online articles will appear here"
+center.appendChild(select)
+(async function() {
+	const onlineArticles = await new Promise(function(res, rej) {
+		fetch("information.json").then(async function(e) {
+			return await e.json()
+		}).then(function(e) {
+			res(e)
+		})
+	})
+
+	input.placeholder = "NOW search an article... we have only " + onlineArticles.pages.slice(0, 5).join(", ") + " for articles."
+	input.addEventListener("keydown", function(event) {
+		if (event.key == "Enter") {
+			input.style.opacity = "1"
+			setInterval(function() {
+				input.style.opacity = String(parseFloat(input.style.opacity) - 0.1)
+			}, 100)
+			setTimeout(function() {
+				input.remove()
+			}, 1000)
+			p.innerText = "Select an article to view it below"
+			onlineArticles.pages.filter(item => item.includes(event.target.value.toLowerCase())).forEach(function(item, i) {
+				if (i === 0) {
+					option.innerText = item
+				} else {
+					const option = document.createElement("option")
+					option.innerText = item
+					select.appendChild(option)
+				}
+			})
+			select.addEventListener("input", function(u) {
+				p.style.opacity = "1"
+				select.style.opacity = "1"
+				setInterval(function() {
+					p.style.opacity = String(parseFloat(p.style.opacity) - 0.1)
+					select.style.opacity = String(parseFloat(select.style.opacity) - 0.1)
+				}, 100)
+				setTimeout(function() {
+					p.remove()
+					select.remove()
+					transitions(onlineArticles.information[u.target.value], false)
+				}, 1000)
+			})
+		}
+	})
+})()
