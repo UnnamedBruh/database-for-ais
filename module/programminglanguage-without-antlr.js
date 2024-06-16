16aa8a4f467272e006e7fb3330fe93440975338a
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-06-16 12:33:52 -0400

    Update programminglanguage-without-antlr.js

--- module/programminglanguage-without-antlr.js
+++ module/programminglanguage-without-antlr.js
@@ -1,5 +1,13 @@
 // You don't HAVE to use ANTLR to make a programming language
 let ProgrammingLanguage = (function() {
-  const ignoreReg = /[\t\r \n;]+/g // Ignore any whitespace characters, including semicolons, since they're not important in our code.
-  function lexer
+	const ignoreReg = /(?<!")[\t\r \n;]+(?!")/g // Ignore any whitespace characters, including semicolons, since they're not important in our code.
+	function lexer(code) {
+		return code.split(ignoreReg)
+	}
+	function parser(tokens) {
+		let transpiled = []
+		let int = /[0-9]+(?!\.)/
+		let num = /[0-9]+(\.([0-9]+)?)/
+		let str = /\"[^\"]*\"/
+	}
 })()
